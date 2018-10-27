d0de48fb38d038494a715d0c2b1665e6178e8c27
Joshua Ohlman <codewriterohs@gmail.com>
2018-10-27 13:00:43 -0500

    Move imports inside of aggregate roots

--- domain/root.js
+++ domain/root.js
@@ -1,10 +1,10 @@
-const attachQuery = require('./helpers/attach-query-to-domain');
-const attachCommand = require('./helpers/attach-command-to-domain');
-const attachSubdomain = require('./helpers/attach-subdomain-to-domain');
-const attachAggregate = require('./helpers/attach-aggregate-to-domain');
-
 module.exports = function makeDomain() {
   const domain = { events: [] };
+
+  const attachQuery = require('./helpers/attach-query-to-domain');
+  const attachCommand = require('./helpers/attach-command-to-domain');
+  const attachSubdomain = require('./helpers/attach-subdomain-to-domain');
+  const attachAggregate = require('./helpers/attach-aggregate-to-domain');
 
   attachQuery(domain, 'ViewAllGrandkids', require('./queries/view-all-grandkids'));
   attachCommand(domain, 'AddGrandkid', require('./commands/add-grandkid'));
