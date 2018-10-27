7687c468188ae61f9d36de83e7cd3ca88c73e2cb
Joshua Ohlman <codewriterohs@gmail.com>
2018-10-27 13:11:51 -0500

    Pull grandkid domain imports into root function

--- domain/domains/grandkid.js
+++ domain/domains/grandkid.js
@@ -1,8 +1,8 @@
-const attachAggregate = require('../helpers/attach-aggregate-to-domain');
-const genericPropertyQuery = require('../aggregates/generic-property-aggregate');
-
 module.exports = function makeGrandkid() {
   const domain = { events: [] };
+
+  const attachAggregate = require('../helpers/attach-aggregate-to-domain');
+  const genericPropertyQuery = require('../aggregates/generic-property-aggregate');
 
   attachAggregate(domain, 'Name', genericPropertyQuery('name'));
 
